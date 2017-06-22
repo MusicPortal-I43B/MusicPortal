8b014f32b92dc7c782c267396ee7512e8fc36c07
Tipputhynhean <tipputhinhean@gmail.com>
2017-06-23 00:13:20 +0700

    - Update all genre - Update audioplayer page to accept song id via GET and play it

--- pages/genres/allGenre.php
+++ pages/genres/allGenre.php
@@ -22,9 +22,17 @@
     <script>
         new WOW().init();
     </script>
+
+    <!-- Audio Player CSS & Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script src="../../public/js/audioPlayerJS/mediaelement-and-player.min.js"></script>
+    <link rel="stylesheet" href="../../public/css/playerStyle.css" media="screen">
+    <!-- end Audio Player CSS & Scripts -->
 </head>
 
 <body>
+
+
 <div class="wrapper">
 
     <div id="mySidenav" class="sidenav">
@@ -48,88 +56,130 @@
     </div>
     <div class="clearfix"></div>
 
-<!--    <div class="col-md-6 col-sm-12 col-xs-12 wow slideInLeft" data-wow-duration="1.5s" data-wow-delay="0.1s">-->
-        <div class="panel panel-mostPopular" style="margin-top: 20px;">
-            <div class="panel-heading"><h2>Popular pop song</h2></div>
-            <div class="panel-body">
-                <?php
-                require_once('../../config/dbconfig.php');
-                $sqlTest = "select * from table_song where song_rating >= 6 AND song_genre = 'POP' LIMIT 25";
-                $result = $conn->query($sqlTest);
-                while ($row = $result->fetch_object()) {
-                    echo "
-                    <div class=\"grow\" style=\"border-top:1px solid #000;\">
-                    <div id=\"status1\" class=\"status\">►</div>
-                    <div class=\"left\"><br><p>$row->song_name</p></div>
-                    <div class=\"right\"><br><p>03:53</p></div>
-                    <br><br>
-                    <button type=\"button\" class=\"btn btn-info\" onclick=\"changeSource('$row->song_directory', 'status1')\">Play</button>
-                    <br>
-                    <div class=\"left\"><br><p>Artist: $row->song_artist</p></div>
-                    <div class=\"right\"><br><p>Release year: $row->song_release_year</p></div>
-                    <p>Album: $row->song_album</p>
+
+    <div class="container">
+        <!-- <h3 class="artiststext">SONGS</h3> -->
+        <div class="scrollmenu col-md-12 col-sm-12 col-xs-12" style="margin: 0px; padding: 0px">
+            <!-- Most popular -->
+            <div class="col-md-6 col-sm-12 col-xs-12 wow slideInLeft" data-wow-duration="1.5s" data-wow-delay="0.1s">
+                <!-- POP -->
+                <div class="panel panel-mostPopular" style="margin-top: 20px;">
+                    <div class="panel-heading"><h2>Popular pop song</h2></div>
+                    <div class="panel-body">
+                        <?php
+                        require_once('../../config/dbconfig.php');
+                        $sqlTest = "select * from table_song where song_rating >= 1 AND song_genre = 'POP' LIMIT 25";
+                        $result = $conn->query($sqlTest);
+                        while ($row = $result->fetch_object()) {
+                            echo "
+                            <div class=\"grow\" style=\"border-top:1px solid #000;\">
+                            <div id=\"status1\" class=\"status\">►</div>
+                            <div class=\"left\"><br><p>$row->song_name</p></div>
+                            <div class=\"right\"><br><p>03:53</p></div>
+                            <br><br>
+                            <button type=\"button\" class=\"btn btn-info\" onclick=\"changeSource('$row->song_directory', 'status1')\">Play</button>
+                            <br>
+                            <div class=\"left\"><br><p>Artist: $row->song_artist</p></div>
+                            <div class=\"right\"><br><p>Release year: $row->song_release_year</p></div>
+                            <p>Album: $row->song_album</p>
+                            </div>
+                            ";
+                        }
+                        ?>
                     </div>
-                    ";
-                }
-                ?>
+                </div>
+
+                <!-- R&B -->
+                <div class="panel panel-mostPopular" style="margin-top: 20px;">
+                    <div class="panel-heading"><h2>Popular R&B song</h2></div>
+                    <div class="panel-body">
+                        <?php
+                        require_once('../../config/dbconfig.php');
+                        $sqlTest = "select * from table_song where song_rating >= 6 AND song_genre = 'R&B' LIMIT 25";
+                        $result = $conn->query($sqlTest);
+                        while ($row = $result->fetch_object()) {
+                            echo "
+                            <div class=\"grow\" style=\"border-top:1px solid #000;\">
+                            <div id=\"status1\" class=\"status\">►</div>
+                            <div class=\"left\"><br><p>$row->song_name</p></div>
+                            <div class=\"right\"><br><p>03:53</p></div>
+                            <br><br>
+                            <button type=\"button\" class=\"btn btn-info\" onclick=\"changeSource('$row->song_directory', 'status1')\">Play</button>
+                            <br>
+                            <div class=\"left\"><br><p>Artist: $row->song_artist</p></div>
+                            <div class=\"right\"><br><p>Release year: $row->song_release_year</p></div>
+                            <p>Album: $row->song_album</p>
+                            </div>
+                            ";
+                        }
+                        ?>
+                    </div>
+                </div>
             </div>
-        </div>
 
-
-
-        <div class="panel panel-mostPopular" style="margin-top: 20px;">
-            <div class="panel-heading"><h2>Popular R&B song</h2></div>
-            <div class="panel-body">
-                <?php
-                require_once('../../config/dbconfig.php');
-                $sqlTest = "select * from table_song where song_rating >= 6 AND song_genre = 'R&B' LIMIT 25";
-                $result = $conn->query($sqlTest);
-                while ($row = $result->fetch_object()) {
-                    echo "
-                    <div class=\"grow\" style=\"border-top:1px solid #000;\">
-                    <div id=\"status1\" class=\"status\">►</div>
-                    <div class=\"left\"><br><p>$row->song_name</p></div>
-                    <div class=\"right\"><br><p>03:53</p></div>
-                    <br><br>
-                    <button type=\"button\" class=\"btn btn-info\" onclick=\"changeSource('$row->song_directory', 'status1')\">Play</button>
-                    <br>
-                    <div class=\"left\"><br><p>Artist: $row->song_artist</p></div>
-                    <div class=\"right\"><br><p>Release year: $row->song_release_year</p></div>
-                    <p>Album: $row->song_album</p>
+            <div class="col-md-6 col-sm-12 col-xs-12 wow slideInLeft" data-wow-duration="1.5s" data-wow-delay="0.1s">
+                <!-- R&B -->
+                <div class="panel panel-mostPopular" style="margin-top: 20px;">
+                    <div class="panel-heading"><h2>Popular Hip Hop song</h2></div>
+                    <div class="panel-body">
+                        <?php
+                        require_once('../../config/dbconfig.php');
+                        $sqlTest = "select * from table_song where song_rating >= 6 AND song_genre = 'Hip-Hop' LIMIT 25";
+                        $result = $conn->query($sqlTest);
+                        while ($row = $result->fetch_object()) {
+                            echo "
+                            <div class=\"grow\" style=\"border-top:1px solid #000;\">
+                            <div id=\"status1\" class=\"status\">►</div>
+                            <div class=\"left\"><br><p>$row->song_name</p></div>
+                            <div class=\"right\"><br><p>03:53</p></div>
+                            <br><br>
+                            
+                            <form method=\"post\" action=\"../../pages/audioPlayer.php\">
+                            <button type=\"submit\" class=\"btn btn-info\" value='$row->song_id' name=\"songID\">Play</button>
+                            </form>
+                            
+                            <br>
+                            <div class=\"left\"><br><p>Artist: $row->song_artist</p></div>
+                            <div class=\"right\"><br><p>Release year: $row->song_release_year</p></div>
+                            <p>Album: $row->song_album</p>
+                            </div>
+                            ";
+                        }
+                        ?>
                     </div>
-                    ";
-                }
-                ?>
+                </div>
+
+                <!-- Rock -->
+                <div class="panel panel-mostPopular" style="margin-top: 20px;">
+                    <div class="panel-heading"><h2>Popular rock song</h2></div>
+                    <div class="panel-body">
+                        <?php
+                        require_once('../../config/dbconfig.php');
+                        $sqlTest = "select * from table_song where song_rating >= 6 AND song_genre = 'Rock' LIMIT 25";
+                        $result = $conn->query($sqlTest);
+                        while ($row = $result->fetch_object()) {
+                            echo "
+                                <div class=\"grow\" style=\"border-top:1px solid #000;\">
+                                <div id=\"status1\" class=\"status\">►</div>
+                                <div class=\"left\"><br><p>$row->song_name</p></div>
+                                <div class=\"right\"><br><p>03:53</p></div>
+                                <br><br>
+                                <button type=\"button\" class=\"btn btn-info\" onclick=\"changeSource('$row->song_directory', 'status1')\">Play</button>
+                                <br>
+                                <div class=\"left\"><br><p>Artist: $row->song_artist</p></div>
+                                <div class=\"right\"><br><p>Release year: $row->song_release_year</p></div>
+                                <p>Album: $row->song_album</p>
+                                </div>
+                                ";
+                        }
+                        ?>
+                    </div>
+                </div>
             </div>
-        </div>
-
-    <div class="panel panel-mostPopular" style="margin-top: 20px;">
-        <div class="panel-heading"><h2>Popular rock song</h2></div>
-        <div class="panel-body">
-            <?php
-            require_once('../../config/dbconfig.php');
-            $sqlTest = "select * from table_song where song_rating >= 6 AND song_genre = 'Rock' LIMIT 25";
-            $result = $conn->query($sqlTest);
-            while ($row = $result->fetch_object()) {
-                echo "
-                    <div class=\"grow\" style=\"border-top:1px solid #000;\">
-                    <div id=\"status1\" class=\"status\">►</div>
-                    <div class=\"left\"><br><p>$row->song_name</p></div>
-                    <div class=\"right\"><br><p>03:53</p></div>
-                    <br><br>
-                    <button type=\"button\" class=\"btn btn-info\" onclick=\"changeSource('$row->song_directory', 'status1')\">Play</button>
-                    <br>
-                    <div class=\"left\"><br><p>Artist: $row->song_artist</p></div>
-                    <div class=\"right\"><br><p>Release year: $row->song_release_year</p></div>
-                    <p>Album: $row->song_album</p>
-                    </div>
-                    ";
-            }
-            ?>
         </div>
     </div>
-<!--    </div>-->
 </div>
+
 
 
 </body>
